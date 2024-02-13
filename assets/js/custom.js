379eef97ff56668c5bb617a7b3051837bf295ef3
Carolina Ruiz <ing.acrd@gmail.com>
2024-02-13 14:23:52 -0500

    contact form functionality

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,131 @@
+function validate_fName() {
+        let fName = document.getElementById('inputFirstName'); 
+        let fNameMessage = document.getElementById('fName-feedback');
+
+        if (fName.value == ""){
+            fNameMessage.classList.remove("error-txt");
+        }
+        else {
+            fNameMessage.classList.add("error-txt");
+        }
+}
+
+function validate_lName() {
+        let lName = document.getElementById('inputLastName'); 
+        let lNameMessage = document.getElementById('lName-feedback');
+
+        if (lName.value == ""){
+            lNameMessage.classList.remove("error-txt");
+        }
+        else {
+            lNameMessage.classList.add("error-txt");
+        }
+}
+
+function validate_email(){
+    console.log("validating email");
+    let inputEmail = document.getElementById('inputEmail'); 
+    let email_feedback = document.getElementById('email-feedback');
+    if (inputEmail.value == ""){
+        email_feedback.classList.remove("error-txt");
+        email_feedback.innerText="email can not be empty";
+    } else {
+        email_feedback.classList.add("error-txt");
+    }
+}
+
+function validate_msg(){
+    let message = document.getElementById('message'); 
+    let msg_feedback = document.getElementById('msg-feedback');
+
+    if (message.value == ""){
+        msg_feedback.classList.remove("error-txt");
+    } else {
+        msg_feedback.classList.add("error-txt");
+    }
+}
+
+function checkInputs() {
+    var valid = true;
+    const email_regex = /[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+    let fName = document.getElementById('inputFirstName'); 
+    let fNameMessage = document.getElementById('fName-feedback');
+
+    let lName = document.getElementById('inputLastName'); 
+    let lNameMessage = document.getElementById('lName-feedback');
+
+    let inputEmail = document.getElementById('inputEmail'); 
+    let email_feedback = document.getElementById('email-feedback');
+    
+    let message = document.getElementById('message'); 
+    let msg_feedback = document.getElementById('msg-feedback');
+
+    if (fName.value == ""){
+        fNameMessage.classList.remove("error-txt");
+        valid = false;
+    }
+    if (lName.value == ""){
+        lNameMessage.classList.remove("error-txt");
+        valid = false;
+    }
+    if (inputEmail.value == ""){
+        console.log("email empty");
+        email_feedback.classList.remove("error-txt");
+        email_feedback.innerText="email can not be empty";
+        valid = false;
+        
+    } else {
+        console.log("email tiene algo");
+        if(!inputEmail.value.match(email_regex)){
+            email_feedback.classList.remove("error-txt");
+            console.log("email no cuadra");
+            email_feedback.innerText="email not valid";
+            valid = false;
+        }
+        else {
+            email_feedback.classList.add("error-txt");
+            console.log("email VALID0!");
+        }
+        
+    }
+    if (message.value == ""){
+        msg_feedback.classList.remove("error-txt");
+        valid = false;
+    }
+    
+    
+
+    return valid;
+}
+
+
+function sendEmail() {
+    // checkInputs();
+    if(checkInputs()){
+        let firstName = document.getElementById("inputFirstName").value;
+        let lastName = document.getElementById("inputLastName").value
+        var params = {
+            from_name: firstName + " " + lastName,
+            email_id: document.getElementById("inputEmail").value,
+            message: document.getElementById("message").value
+        }
+
+        emailjs.send("service_tz90wy8","template_8ow4z4a", params).then(function(res) {
+            
+            if (res.status == 200){
+                Swal.fire({
+                    title: "Message sent!",
+                    text: "Thanks for your message, I'll be in touch soon.",
+                    icon: "success"
+                    });
+            }
+        });
+
+    }
+    
+}
+
 function changeColor(color) {
     //change-color
     var elements = document.querySelectorAll('.change-color');
@@ -67,8 +195,6 @@ function changeColor(color) {
 
     }
  
-    
-
 
 }
 
